160c4a578a2701381c31d5291aaf68b67c1e8b24
tomas.roch <rochtomass@gmail.com>
2022-10-28 12:34:14 +0200

    initialize version

---
+++ src/main/java/com/pizza/pizza_reservation/PizzaReservationApplication.java
@@ -0,0 +1,13 @@
+package com.pizza.pizza_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaReservationApplication.class, args);
+    }
+
+}
