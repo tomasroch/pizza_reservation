0a0e76511e4e0410066626e8e69f95293a5aa970
tomas.roch <rochtomass@gmail.com>
2022-11-06 11:20:42 +0100

    fix pizza ingredient

--- server/src/main/java/com/pizza/pizza_reservation/domain/PizzaIngredient.java
+++ server/src/main/java/com/pizza/pizza_reservation/domain/PizzaIngredient.java
@@ -2,8 +2,10 @@ package com.pizza.pizza_reservation.domain;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
+@IdClass(PizzaIngredient.PizzaIngredientPK.class)
 public class PizzaIngredient implements Serializable {
 
   @Id
@@ -26,6 +28,39 @@ public class PizzaIngredient implements Serializable {
 
   public void setIngredientId(Integer ingredientId) {
     this.ingredientId = ingredientId;
+  }
+
+  public class PizzaIngredientPK implements Serializable {
+
+    private Integer pizzaId;
+    private Integer ingredientId;
+
+    public PizzaIngredientPK(Integer pizzaId, Integer ingredientId) {
+      this.pizzaId = pizzaId;
+      this.ingredientId = ingredientId;
+    }
+    public PizzaIngredientPK() {
+    }
+
+    //Getters and setters are omitted for brevity
+
+    @Override
+    public boolean equals(Object o) {
+      if ( this == o ) {
+        return true;
+      }
+      if ( o == null || getClass() != o.getClass() ) {
+        return false;
+      }
+      PizzaIngredientPK pk = (PizzaIngredientPK) o;
+      return Objects.equals( pizzaId, pk.pizzaId ) &&
+              Objects.equals( ingredientId, pk.ingredientId );
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash( pizzaId, ingredientId );
+    }
   }
 
 }
