390f8f1db83b9edf79226d1530f11defaf17ab24
tomas.roch <rochtomass@gmail.com>
2023-01-17 18:37:40 +0100

    testy

--- server/src/test/java/com/pizza/pizza_reservation/PizzaControllerTests.java
+++ server/src/test/java/com/pizza/pizza_reservation/PizzaControllerTests.java
@@ -32,4 +32,30 @@ public class PizzaControllerTests extends ControllerTestAncestor {
         System.out.println(json.toString(4));
     }
 
+    @Test
+    public void getAllActivePizzasTest() throws Exception {
+        String uri = "/api/pizza/list/active/{active}";
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri,false)
+                                                                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(OK, status);
+        assertNotNull(mvcResult.getResponse().getContentAsString());
+        JSONArray json = new JSONArray(mvcResult.getResponse().getContentAsString());
+        System.out.println(json.toString(4));
+    }
+
+    @Test
+    public void getAllPizzaByIdTest() throws Exception {
+        String uri = "/api/pizza/{id}";
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri,25)
+                                                                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(OK, status);
+        assertNotNull(mvcResult.getResponse().getContentAsString());
+        JSONObject json = new JSONObject(mvcResult.getResponse().getContentAsString());
+        System.out.println(json.toString(4));
+    }
+
 }
