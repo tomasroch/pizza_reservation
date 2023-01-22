dd70018c7efe385fae5928a462010b814e92e6da
tomas.roch <rochtomass@gmail.com>
2023-01-22 11:01:01 +0100

    ...

--- server/src/main/java/com/pizza/pizza_reservation/repository/PizzaOrderRepository.java
+++ server/src/main/java/com/pizza/pizza_reservation/repository/PizzaOrderRepository.java
@@ -3,7 +3,6 @@ package com.pizza.pizza_reservation.repository;
 import com.pizza.pizza_reservation.domain.PizzaOrder;
 import com.pizza.pizza_reservation.domain.idclass.PizzaOrderId;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -12,7 +11,6 @@ import java.util.List;
 @Repository
 public interface PizzaOrderRepository extends JpaRepository<PizzaOrder, PizzaOrderId> {
 
-    @Modifying
     @Query("SELECT po from PizzaOrder po where po.orderId = ?1")
     List<PizzaOrder> readOrderItems(Integer id);
 }
